79ca9c8bce7fd1444ec9f4106c7248f567690472
khangvu <vumanhkhang1102@gmail.com>
2017-07-07 17:30:45 +0700

    [view] change something in view.

--- controllers/user.js
+++ controllers/user.js
@@ -3,20 +3,24 @@ var router = express.Router();
 var passport = require('passport');
 var Userdata = require('../models/user');
 var localStrategy = require('passport-local').Strategy;
-router.post('/user',Userdata.addUser);
-router.get('/user',Userdata.getAllUser);
-router.delete('/delete/:id',Userdata.deleteUser);
-router.put('/update/:id',Userdata.updateUser);
-router.get('/user/:id',Userdata.getUserById);
-router.get('/',function(req,res){
-    console.log("aaaaaaaaaaa");
+router.post('/user', Userdata.addUser);
+router.get('/user', Userdata.getAllUser);
+router.delete('/delete/:id', Userdata.deleteUser);
+router.put('/update/:id', Userdata.updateUser);
+router.get('/user/:id', Userdata.getUserById);
+router.get('/', function (req, res) {
+
     res.render('index');
 });
-router.get('/login',function(req,res){
-    console.log("aaaaaaaaaaa");
+router.get('/login', function (req, res) {
+
     res.render('login');
 });
-router.get('/register', function(req, res){
-	res.render('register');
+router.get('/profile', function (req, res) {
+
+    res.render('profile');
+});
+router.get('/register', function (req, res) {
+    res.render('register');
 });
 module.exports = router;
