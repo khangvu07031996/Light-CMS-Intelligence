a5484ea64a610cf5042de48e556265404f31ae82
khangvu <vumanhkhang1102@gmail.com>
2017-08-22 10:21:05 +0700

    [unit test]

--- test/test.js
+++ test/test.js
@@ -5,11 +5,7 @@ const should = require('should');
 
 const server = supertest.agent('http://localhost:3000');
 
-// UNIT test begin
-
 describe('Test restful API', () => {
-  // #1 should return home page
-
   it('should return a 200 response', (done) => {
     server
       .get('/api/articles')
@@ -38,9 +34,9 @@ describe('Test restful API', () => {
         status: 'published',
         CreateBy: 'khangvu' })
       .expect('Content-type', /json/)
-      .expect(201)
+      .expect(200)
       .end((err, res) => {
-        res.status.should.equal(500);
+        article = res.body;
         done();
       });
   });
@@ -50,6 +46,36 @@ describe('Test restful API', () => {
       .expect(404)
       .end((err, res) => {
         res.status.should.equal(404);
+        done();
+      });
+  });
+  it('update an article', (done) => {
+    server
+      .put('/api/articles/599b9cf72f4b372f386d37c5')
+      .set('Acept', 'application/x-ww-form-urlencoded')
+      .send({
+        headline: 'khang vu123123123213123123',
+        section: 'khang213213123',
+        premble: '123',
+        body: '123',
+        images: '123.png',
+        author: 'asdas',
+        tags: 'asdasd',
+        widgets: 'asda',
+        date_created: '2017-08-14T06:47:02.275Z',
+        publishDate: '2017-08-14T06:47:02.275Z',
+        status: 'published',
+        CreateBy: 'khangvu' })
+      .expect(200)
+      .end((err, res) => {
+        done();
+      });
+  });
+  it('delete an article', (done) => {
+    server
+      .delete('/api/articles/599b9cf72f4b372f386d37c5')
+      .expect(200)
+      .end((err, res) => {
         done();
       });
   });
